a1ace8c710782ed2d3c9e689ad9a909cb049ef64
unknown <mannyeslexpert@gmail.com>
2021-06-23 18:20:16 -0300

    construindo formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
+import CadastroClientes from "./pages/CadastroClientes";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Olá</h1>
-    </div>
+    //<div className="App">
+      //<h1>Olá</h1>
+    //</div>
+    <CadastroClientes></CadastroClientes>
   );
 }
 
+
 export default App;
+
+
---
+++ src/pages/CadastroClientes.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react"
+import { DivBox, DivEspaco1, DivEspaco2, Section, InputData, Button } from "../styles/forms-styles";
+
+
+const CadastroClientes = () => {
+    const [email, setEmail] = useState("");
+    const [userName, setUserName] = useState("");
+    const [senha, setSenha] = useState("");
+    const [nome, setNome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [dataNascimento, setDataNascimento] = useState("");
+
+    const enviarDados = () => {
+        
+        
+        let dadosCliente = {
+            email: email,
+            userName: userName,
+            senha: senha,
+            nome: nome,
+            cpf: cpf,
+            telefone: telefone,
+            dataNascimento: dataNascimento
+
+        }
+        
+        setEmail("")
+        setUserName("")
+        setSenha("")
+        setNome("")
+        setCpf("")
+        setTelefone("")
+        setDataNascimento("")
+    }
+    return (
+        <Section>
+            <DivBox>
+                <DivEspaco1>
+                    <label htmlFor="email">E-mail</label>
+                    <InputData id="email"  type="email" placeholder="seuemail@provedor.com"value={email} onChange={(e) => setEmail(e.target.value)}></InputData>
+                </DivEspaco1>
+                <DivEspaco2>
+                    <DivEspaco1>
+                        <label htmlFor="userName">User Name</label>
+                        <InputData id="userName" type="text" placeholder="nickname"value={userName} onChange={(e) => setUserName(e.target.value)}></InputData>
+                    </DivEspaco1>
+                    <DivEspaco1>
+                        <label htmlFor="senha">Senha</label>
+                        <InputData id="senha" type="password" placeholder="******"value={senha} onChange={(e) => setSenha(e.target.value)}></InputData>
+                    </DivEspaco1>
+                </DivEspaco2>
+                <DivEspaco2>
+                    <DivEspaco1>
+                        <label htmlFor="nome">Nome</label>
+                        <InputData id="nome" type="text" placeholder="seu nome"value={nome} onChange={(e) => setNome(e.target.value)}></InputData>
+                    </DivEspaco1>
+                    <DivEspaco1>
+                        <label htmlFor="cpf">CPF</label>
+                        <InputData id="cpf" type="number" placeholder="123456789-00" value={cpf} onChange={(e) => setCpf(e.target.value)}></InputData>
+                    </DivEspaco1>
+                </DivEspaco2>
+                <DivEspaco2>
+                    <DivEspaco1>
+                        <label htmlFor="dataNascimento">Nascimento</label>
+                        <InputData id="dataNascimento" type="date"value={dataNascimento} onChange={(e) => setDataNascimento(e.target.value)}></InputData>
+                       
+                    </DivEspaco1>
+                    <DivEspaco1>
+                    <label htmlFor="telefone">Telefone</label>
+                        <InputData id="telefone" type="telephone" placeholder="00 9988-7766" value={telefone} onChange={(e) => setTelefone(e.target.value)}></InputData>
+                    </DivEspaco1>
+
+                </DivEspaco2>
+                <DivEspaco1>
+                    <label></label>
+                    <Button onClick = {()=> {enviarDados ()}}>Cadastrar</Button>
+
+                </DivEspaco1>
+
+
+
+            </DivBox>
+            </Section>
+        
+    );
+}
+export default CadastroClientes;
+
