5ab15db6ad63b46fc51e062b5ebdbc19a72af3e5
RodrigoBR-dev <rodrigocmartins@hotmail.com>
2021-06-23 20:24:17 -0300

    limpeza do merge

--- src/utils/money.js
+++ src/utils/money.js
@@ -2,8 +2,5 @@ function formatarParaReal(valor){
     return Number(valor).toLocaleString('pt-BR', {style:'currency', currency:'BRL'});
 }
 
-<<<<<<< HEAD
 export default formatarParaReal;
-=======
-export default formatarParaReal; 
->>>>>>> c9cf63a4bafc8819c1d1b3a54589b31c56921aaa
+
